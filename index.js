55d2d4cdf26eaa347ec8cc13812b234648b8e11f
NewYoung <657851574@qq.com>
2018-03-16 20:02:58 +0800

    小修改

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
     /**
      *
      * @param title 组件标题
-     * @param src   图片路径
+     * @param src   初始图片路径
      * @param size  裁剪区域实际尺寸以及相对于裁剪容器位置
      * @param times 实际尺寸/显示尺寸
      * @param maskSize 裁剪容器实际尺寸
@@ -35,8 +35,9 @@
         this.maskSize.height = 600*this.times;
         this.scaleTimes = 1;
 
-        this.size.left = (this.maskSize.width-this.size.width)/2;
-        this.size.top = (this.maskSize.height-this.size.height)/2;
+        this.size.left = (this.maskSize.width-this.size.width)*1.0/2;
+        this.size.top = (this.maskSize.height-this.size.height)*1.0/2;
+        this.borderWidth = 2;
 
         this.construct();
         this.load();
@@ -68,23 +69,21 @@
         html += '</div>';
         html += '</div>';
 
-        var $target = document.createElement('div');
-        $target.id = this.id;
-        $target.classList.add('crop-whole-cover');
-        $target.innerHTML = html;
-
-        this.$target = $target;
-        document.body.appendChild($target);
+        this.$target = document.createElement('div');
+        this.$target.id = this.id;
+        this.$target.classList.add('crop-whole-cover');
+        this.$target.innerHTML = html;
+        document.body.appendChild(this.$target);
 
         var $cropCover = document.querySelector('#'+this.id+' .crop-cover');
         var cropCoverContext = $cropCover.getContext('2d');
         cropCoverContext.fillStyle = 'rgba(0,0,0,.3)';
         cropCoverContext.fillRect(0,0,$cropCover.width,$cropCover.height);
         cropCoverContext.fillStyle = '#0BFF00';
-        var width = 4*this.times+this.size.width;
-        var height = 4*this.times+this.size.height;
-        cropCoverContext.fillRect((this.maskSize.width-width)/2,(this.maskSize.height-height)/2,width,height);
-        cropCoverContext.clearRect((this.maskSize.width-this.size.width)/2,(this.maskSize.height-this.size.height)/2,this.size.width,this.size.height);
+        var width = this.borderWidth*2*this.times+this.size.width;
+        var height = this.borderWidth*2*this.times+this.size.height;
+        cropCoverContext.fillRect((this.maskSize.width-width)*1.0/2,(this.maskSize.height-height)*1.0/2,width,height);
+        cropCoverContext.clearRect((this.maskSize.width-this.size.width)*1.0/2,(this.maskSize.height-this.size.height)*1.0/2,this.size.width,this.size.height);
     };
 
     //加载图片
@@ -101,6 +100,7 @@
                 width:self.$image.width,
                 height:self.$image.height
             };
+            self.cropContentContext.clearRect(0,0,self.maskSize.width,self.maskSize.height);
             self.cropContentContext.drawImage(self.$image,self.contentRect.left,self.contentRect.top,self.$image.width,self.$image.height);
         }
     };
@@ -109,6 +109,8 @@
     SimpleCrop.prototype.bindEvent = function(){
         //获取事件相关dom元素
         var self = this;
+        self.$uploadBtn = document.querySelector('#'+self.id+' .upload-btn-container');
+        self.$closeBtn = document.querySelector('#'+self.id+' .crop-close');
         self.$scaleBtn = document.querySelector('#'+self.id+' .scale-btn');
         self.$scaleNum = document.querySelector('#'+self.id+' .scale-num');
         self.$scaleOneTimes = document.querySelector('#'+self.id+' .one-times-icon');
@@ -171,7 +173,7 @@
                 }
                 if(isChanged){
                     self.contentRect = newContentRect;
-                    self.cropContentContext.clearRect(0,0,self.$cropContent.width,self.$cropContent.height);
+                    self.cropContentContext.clearRect(0,0,self.maskSize.width,self.maskSize.height);
                     self.cropContentContext.drawImage(self.$image,self.contentRect.left,self.contentRect.top,self.$image.width,self.$image.height);
                 }
                 self.downPoint = point;
@@ -223,6 +225,22 @@
         self.$scaleTwoTimes.addEventListener('click',function(ev){
             self.scaleMove(self.scaleWidth);
         },false);
+
+        //点击关闭
+        self.$closeBtn.addEventListener('click',function(){
+            self.$target.style.display = 'none';
+        },false);
+
+        //重新上传
+        self.$uploadBtn.addEventListener('change',function(evt){
+            var files = evt.target.files;
+            if(files.length>0){
+                self.fileToSrc(files[0],function(src){
+                    self.src = src;
+                    self.load();
+                });
+            }
+        },false);
     };
 
     //滑动按钮移动
@@ -238,7 +256,7 @@
         coverTect = this.rectLimit(coverTect);
         this.contentRect = this.coverRectToContentRect(coverTect);
 
-        this.cropContentContext.clearRect(0,0,this.$cropContent.width,this.$cropContent.height);
+        this.cropContentContext.clearRect(0,0,this.maskSize.width,this.maskSize.height);
         this.cropContentContext.drawImage(this.$image,this.contentRect.left,this.contentRect.top,this.$image.width,this.$image.height);
     };
 
@@ -250,8 +268,8 @@
             width:contentRect.width*this.scaleTimes,
             height:contentRect.height*this.scaleTimes
         };
-        var overLeft = this.maskSize.width*(this.scaleTimes-1)/2;
-        var overTop = this.maskSize.height*(this.scaleTimes-1)/2;
+        var overLeft = this.maskSize.width*(this.scaleTimes-1)*1.0/2;
+        var overTop = this.maskSize.height*(this.scaleTimes-1)*1.0/2;
 
         coverRect.left = coverRect.left-overLeft;
         coverRect.top = coverRect.top-overTop;
@@ -259,12 +277,12 @@
         return coverRect;
     };
     SimpleCrop.prototype.coverRectToContentRect = function(coverRect){
-        var overLeft = this.maskSize.width*(this.scaleTimes-1)/2;
-        var overTop = this.maskSize.height*(this.scaleTimes-1)/2;
+        var overLeft = this.maskSize.width*(this.scaleTimes-1)*1.0/2;
+        var overTop = this.maskSize.height*(this.scaleTimes-1)*1.0/2;
 
         var contentRect = {
-            left:(coverRect.left+overLeft)/this.scaleTimes,
-            top:(coverRect.top+overTop)/this.scaleTimes,
+            left:(coverRect.left+overLeft)*1.0/this.scaleTimes,
+            top:(coverRect.top+overTop)*1.0/this.scaleTimes,
             width:coverRect.width*1.0/this.scaleTimes,
             height:coverRect.height*1.0/this.scaleTimes
         }
@@ -274,18 +292,28 @@
 
     //坐标限制
     SimpleCrop.prototype.rectLimit = function(coverRect){
-        if(coverRect.left>this.size.left){
+        var maxLen = this.maskSize.width>this.maskSize.height?this.maskSize.width:this.maskSize.height;
+        if(coverRect.left>=this.size.left||Math.abs(coverRect.left-this.size.left)<=maxLen*1.0/50){
             coverRect.left = this.size.left;
-        }else if((coverRect.left+coverRect.width)<(this.size.left+this.size.width)){
+        }else if((coverRect.left+coverRect.width)<(this.size.left+this.maskSize.width)){
             coverRect.left = this.size.left+this.size.width-coverRect.width;
         }
-        if(coverRect.top>this.size.top){
+        if(coverRect.top>=this.size.top||Math.abs(coverRect.top-this.size.top)<=maxLen*1.0/50){
             coverRect.top = this.size.top;
         }else if((coverRect.top+coverRect.height)<(this.size.top+this.size.height)){
             coverRect.top = this.size.top+this.size.height-coverRect.height;
         }
 
         return coverRect;
+    };
+
+    //file转image
+    SimpleCrop.prototype.fileToSrc = function(file,callback){
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            callback(e.target.result);
+        }
+        reader.readAsDataURL(file)
     };
 
     return SimpleCrop;
