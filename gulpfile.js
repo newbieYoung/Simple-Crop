5a5df5f8532feba0afae23bcfda2a0ed8ffcd132
newyoungli <newyoungli@tencent.com>
2018-07-13 20:21:24 +0800

    小修改

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var cssBase64 = require('gulp-css-base64');
 
 gulp.task('css-img-base64', function () {
-    return gulp.src('./template.css')
+    return gulp.src('./template-1.css')
         .pipe(cssBase64())
         .pipe(gulp.dest('./dist'));
 });
