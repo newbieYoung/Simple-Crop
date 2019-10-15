0ef76c88e223e87766c20d3a566eca77a20f42cb
NewYoung <657851574@qq.com>
2019-10-15 14:32:07 +0800

    字体文件精简

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
-var cssBase64 = require('gulp-css-base64');
+var base64 = require('gulp-base64');
 
-gulp.task('css-img-base64', function () {
+gulp.task('css-base64', function () {
     gulp.src('./template-1.css')
-        .pipe(cssBase64())
+        .pipe(base64())
         .pipe(gulp.dest('./dist'));
 
     gulp.src('./template-2.css')
-        .pipe(cssBase64())
+        .pipe(base64())
         .pipe(gulp.dest('./dist'));
 });
