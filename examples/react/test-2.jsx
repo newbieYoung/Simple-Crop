1c2604409b3d1473b415c4a98f2de0ce47b9890e
Young <657851574@qq.com>
2020-02-18 18:23:31 +0800

    react 更新

--- examples/react/test-2.jsx
+++ examples/react/test-2.jsx
@@ -9,7 +9,7 @@ class Test2 extends React.Component {
 
     this.state = {
       cropParams: {
-        visible: true, //是否显示
+        visible: false, //是否显示
         src: "https://newbieyoung.github.io/Simple-Crop/img/test2.jpg",
         size: {
           width: 1000,
@@ -21,15 +21,32 @@ class Test2 extends React.Component {
     };
   }
 
+  componentDidMount () {
+    let self = this;
+    setTimeout(function () {
+      let cropParams = self.state.cropParams;
+      cropParams.visible = true;
+      cropParams.src = "https://newbieyoung.github.io/Simple-Crop/img/test1.jpg";
+      //cropParams.rotateSlider = false;
+      cropParams.startAngle = -145;
+      cropParams.endAngle = 75;
+      cropParams.gapAngle = 1;
+      cropParams.lineationItemWidth = 60;
+      self.setState({
+        cropParams: cropParams
+      })
+    }, 2000);
+  }
+
   //图片裁剪回调函数
-  cropCallback() {
+  cropCallback () {
     this.$resultCanvas.style.marginRight = "10px";
     this.$resultCanvas.style.width = "50%";
     document.body.appendChild(this.$resultCanvas);
   }
 
   //设置裁剪图片并显示
-  setCropImg() {
+  setCropImg () {
     let cropParams = this.state.cropParams;
     cropParams.src = "https://newbieyoung.github.io/Simple-Crop/img/test1.jpg";
     cropParams.visible = true;
@@ -38,7 +55,7 @@ class Test2 extends React.Component {
     });
   }
 
-  render() {
+  render () {
     return (
       <div>
         <button onClick={this.setCropImg.bind(this)}>设置裁剪图片并显示</button>
--- index-react.jsx
+++ index-react.jsx
@@ -13,117 +13,70 @@ export class SimpleCrop extends React.Component {
     this.instance = new Core(this.props);
   }
 
-  //组件属性更新是否需要重新构建组件
-  shouldRebuild (prevProps) {
-    //title、src、debug、zIndex、noBoldCorner、coverColor、borderColor、borderWidth、coverDraw、borderDraw、visible、cropCallback、uploadCallback、closeCallback
-    let build = false;
-    if (
-      prevProps.maxScale != this.props.maxScale ||
-      prevProps.scaleSlider != this.props.scaleSlider ||
-      prevProps.cropSizePercent != this.props.cropSizePercent ||
-      prevProps.rotateSlider != this.props.rotateSlider ||
-      prevProps.startAngle != this.props.startAngle ||
-      prevProps.endAngle != this.props.endAngle ||
-      prevProps.gapAngle != this.props.gapAngle ||
-      prevProps.lineationItemWidth != this.props.lineationItemWidth ||
-      prevProps.funcBtns != this.props.funcBtns
-    ) {
-      build = true;
-    }
-
-    if (this.props.size && prevProps.size) {
-      if (
-        prevProps.size.width != this.props.size.width ||
-        prevProps.size.height != this.props.size.height
-      ) {
-        build = true;
-      }
-    } else if (this.props.size || prevProps.size) {
-      build = true;
-    }
-
-    if (this.props.positionOffset && prevProps.positionOffset) {
-      if (
-        prevProps.positionOffset.top != this.props.positionOffset.top ||
-        prevProps.positionOffset.left != this.props.positionOffset.left
-      ) {
-        build = true;
-      }
-    } else if (this.props.positionOffset || prevProps.positionOffset) {
-      build = true;
-    }
-
-    //功能按钮顺序也不能发生变化
-    if (this.props.funcBtns && prevProps.funcBtns) {
-      for (let i = 0; i < this.props.funcBtns.length; i++) {
-        if (prevProps.funcBtns[i] != this.props.funcBtns[i]) {
-          build = true;
-        }
-      }
-    }
-
-    return build;
-  }
-
   //更新
   componentDidUpdate (prevProps) {
-    if (this.shouldRebuild(prevProps)) {
-      if (this.instance) {
-        this.instance.$container.removeChild(this.instance.$target);
-      }
-      this.instance = new Core(this.props);
-    } else {
-      if (this.instance) {
-        if (this.props.visible) {
-          this.instance.show();
-        } else {
-          this.instance.hide();
-        }
-        if (prevProps.title != this.props.title) {
-          this.instance.title = this.props.title;
-          this.instance.$title.innerText = this.props.title;
-        }
-        if (prevProps.src != this.props.src) {
-          this.instance.setImage(this.props.src);
-        }
-        this.instance.debug = this.props.debug;
-        if (prevProps.zIndex != this.props.zIndex) {
-          this.instance.zIndex = this.props.zIndex;
-          this.instance.$target.style.zIndex = this.props.zIndex;
-        }
-        if (prevProps.borderDraw != this.props.borderDraw) {
-          this.instance.borderDraw = this.props.borderDraw.bind(this.instance);
-        }
-        if (
-          prevProps.noBoldCorner != this.props.noBoldCorner ||
-          prevProps.coverColor != this.props.coverColor ||
-          prevProps.borderColor != this.props.borderColor ||
-          prevProps.borderWidth != this.props.borderWidth
-        ) {
-          this.instance.borderDraw();
-        }
-        if (prevProps.coverDraw != this.props.coverDraw) {
-          this.instance.coverDraw = this.props.coverDraw.bind(this.instance);
-          this.instance.borderDraw();
-          this.instance.coverDraw();
-        }
-        if (prevProps.cropCallback != this.props.cropCallback) {
-          this.instance.cropCallback = this.props.cropCallback.bind(
-            this.instance
-          );
-        }
-        if (prevProps.uploadCallback != this.props.uploadCallback) {
-          this.instance.uploadCallback = this.props.uploadCallback.bind(
-            this.instance
-          );
-        }
-        if (prevProps.closeCallback != this.props.closeCallback) {
-          this.instance.closeCallback = this.props.closeCallback.bind(
-            this.instance
-          );
-        }
+    if (this.hasChanged(['src', prevProps])) {
+      this.instance.setImage(this.props.src);
+    }
+
+    if (this.hasChanged(['rotateSlider', 'startAngle', 'endAngle', 'gapAngle', 'lineationItemWidth', prevProps])) {
+      this.instance.initRotateSlider({
+        rotateSlider: this.props.rotateSlider,
+        startAngle: this.props.startAngle,
+        endAngle: this.props.endAngle,
+        gapAngle: this.props.gapAngle,
+        lineationItemWidth: this.props.lineationItemWidth
+      });
+    }
+
+    if (this.hasChanged(['cropSizePercent'], prevProps)
+      || !this.isEquivalent(this.props.positionOffset, prevProps.positionOffset)
+      || !this.isEquivalent(this.props.size, prevProps.size)) {
+      this.instance.updateFrame({
+        size: this.props.size,
+        positionOffset: this.props.positionOffset,
+        cropSizePercent: this.props.cropSizePercent
+      })
+    }
+
+    if (this.hasChanged(['visible'], prevProps)) {
+      if (this.props.visible) {
+        this.instance.show();
+      } else {
+        this.instance.hide();
       }
     }
+  }
+
+  //属性是否发生变化
+  hasChanged (names, props) {
+    let cur = {};
+    let prev = {};
+    for (let i = 0; i < names; i++) {
+      let item = names[i];
+      cur[item] = this.props[item];
+      prev[item] = props[item];
+    }
+    return this.isEquivalent(cur, prev);
+  }
+
+  //根据两个简单对象的值比较它们是否相同
+  isEquivalent (a, b) {
+    var aProps = Object.getOwnPropertyNames(a);
+    var bProps = Object.getOwnPropertyNames(b);
+
+    if (aProps.length != bProps.length) {
+      return false;
+    }
+
+    for (var i = 0; i < aProps.length; i++) {
+      var propName = aProps[i];
+      if (a[propName] !== b[propName]) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   render () {
