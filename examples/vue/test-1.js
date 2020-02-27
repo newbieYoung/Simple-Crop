4ddaf9d3637139faa49a20e4f8bddd7e35808697
Young <657851574@qq.com>
2020-02-28 02:38:02 +0800

    vue 版本初步提交

--- examples/vue/test-1.js
+++ examples/vue/test-1.js
@@ -1,11 +1,54 @@
 import Vue from 'vue'
+import '../../dist/template-1.css'
 import SimpleCrop from '../../index.vue';
+
 new Vue({
   el: '#app',
   data: {
-    name: 'liy'
+    cropParams: {
+      title: "上传图片过大，请裁剪",
+      src: "../../img/test2.jpg",
+      size: {
+        width: 1000,
+        height: 600
+      },
+      cropSizePercent: 0.65,
+      scaleSlider: true,
+      maxScale: 3,
+      borderWidth: 1,
+      funcBtns: ["close", "crop", "upload"],
+      borderColor: "#fff",
+      coverColor: "rgba(0,0,0,.5)",
+      startAngle: -360,
+      endAngle: 360,
+    }
   },
   components: {
     'simple-crop': SimpleCrop
+  },
+  methods: {
+    //组件更新
+    updateComponent() {
+      this.cropParams = JSON.parse(JSON.stringify(this.cropParams)); //改变对象引用
+      this.cropParams.borderColor = '#0BFF00'; //更新值
+      this.cropParams.cropSizePercent = 0.5;
+      this.cropParams.size = {
+        width: 600,
+        height: 600
+      };
+    },
+
+    //设置裁剪图片
+    setCropImage() {
+      this.cropParams = JSON.parse(JSON.stringify(this.cropParams)); //改变对象引用
+      this.cropParams.src = "../../img/test1.jpg";
+    },
+
+    //图片裁剪回调函数
+    cropCallback($resultCanvas) {
+      $resultCanvas.style.marginRight = "10px";
+      $resultCanvas.style.width = "50%";
+      document.body.appendChild($resultCanvas);
+    }
   }
 });
--- index.vue
+++ index.vue
@@ -1,19 +1,176 @@
-<template>
-  <p>{{greeting}} vue</p>
-</template>
+<template></template>
 
 <script>
-console.log("log");
+import Core from "./index.js";
+
 export default {
-  data: function() {
-    return {
-      greeting: "hello"
-    };
+  _instance: null, //组件实例
+
+  props: ["cropParams"],
+
+  mounted() {
+    this.cropParams.cropCallback = this.cropCallback.bind(this);
+    this.instance = new Core(this.cropParams);
   },
-  created: function() {
-    console.log("created");
+
+  methods: {
+    //裁剪回调
+    cropCallback() {
+      this.$emit("crop", this.instance.$resultCanvas);
+    },
+    //属性是否发生变化
+    hasChanged(names, props) {
+      let cur = {};
+      let prev = {};
+      for (let i = 0; i < names.length; i++) {
+        let item = names[i];
+        cur[item] = this.cropParams[item];
+        prev[item] = props[item];
+      }
+      return !this.isEquivalent(cur, prev, names);
+    },
+
+    //根据两个简单对象的值比较它们是否相同
+    isEquivalent(a, b, include) {
+      if (a != null && b != null) {
+        let aProps = [];
+        let aTemps = Object.getOwnPropertyNames(a);
+        let bProps = [];
+        let bTemps = Object.getOwnPropertyNames(b);
+
+        //只考虑 include
+        if (include instanceof Array) {
+          // 数组
+          for (let i = 0; i < include.length; i++) {
+            let item = include[i];
+            if (aTemps.includes(item)) {
+              aProps.push(item);
+            }
+            if (bTemps.includes(item)) {
+              bProps.push(item);
+            }
+          }
+        } else if (include instanceof RegExp) {
+          //正则
+          for (let i = 0; i < aTemps.length; i++) {
+            let item = aTemps[i];
+            if (include.test(item)) {
+              aProps.push(item);
+            }
+          }
+          for (let i = 0; i < bTemps.length; i++) {
+            let item = bTemps[i];
+            if (include.test(item)) {
+              bProps.push(item);
+            }
+          }
+        } else {
+          aProps = aTemps;
+          bTemps = bTemps;
+        }
+
+        if (aProps.length != bProps.length) {
+          return false;
+        }
+
+        for (let i = 0; i < aProps.length; i++) {
+          let propName = aProps[i];
+          if (a[propName] !== b[propName]) {
+            return false;
+          }
+        }
+
+        return true;
+      } else if (a === b) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+
+  //监听参数变化
+  watch: {
+    cropParams(val, oldVal) {
+      if (this.instance) {
+        if (this.hasChanged(["src"], oldVal)) {
+          this.instance.setImage(this.cropParams.src);
+        }
+
+        if (
+          this.hasChanged(
+            [
+              "rotateSlider",
+              "startAngle",
+              "endAngle",
+              "gapAngle",
+              "lineationItemWidth"
+            ],
+            oldVal
+          )
+        ) {
+          this.instance.initRotateSlider(this.cropParams);
+        }
+
+        if (
+          this.hasChanged(["cropSizePercent"], oldVal) ||
+          !this.isEquivalent(
+            this.cropParams.positionOffset,
+            oldVal.positionOffset,
+            ["top", "left"]
+          ) ||
+          !this.isEquivalent(this.cropParams.size, oldVal.size, [
+            "width",
+            "height"
+          ])
+        ) {
+          this.instance.updateBox(this.cropParams);
+        }
+
+        if (
+          this.hasChanged(
+            [
+              "borderWidth",
+              "borderColor",
+              "boldCornerLen",
+              "coverColor",
+              "borderDraw",
+              "coverDraw"
+            ],
+            oldVal
+          )
+        ) {
+          this.instance.initBoxBorder(this.cropParams);
+        }
+
+        if (
+          !this.isEquivalent(
+            this.cropParams.funcBtns,
+            oldVal.funcBtns,
+            /^[0-9]*$/
+          )
+        ) {
+          this.instance.initFuncBtns(this.cropParams);
+        }
+
+        if (this.hasChanged(["scaleSlider", "maxScale"], oldVal)) {
+          this.instance.initScaleSlider(this.cropParams);
+        }
+
+        if (this.hasChanged(["title"], oldVal)) {
+          this.instance.initTitle(this.cropParams);
+        }
+
+        if (this.cropParams.visible == false) {
+          this.instance.hide();
+        } else {
+          this.instance.show();
+        }
+      }
+    }
   }
 };
 </script>
 
-<style lang="stylus" scoped></style>
+<style scoped>
+</style>
